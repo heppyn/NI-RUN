2ac4db7e0f0c088edb7a9dd969ae75fbd385d7ea
Lukáš Hepner <hepneluk@fit.cvut.cz>
2021-03-01 19:11:13 +0100

    Add src

--- fml_ast_interpreter/src/main.cpp
+++ fml_ast_interpreter/src/main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-    std::string_view filePath = "../parsed.json";
+    std::string_view filePath = "../../parsed.json";
     if (argc >= 2) {
         if (strcmp(argv[1], "--help") == 0) {
             std::cout << "Run with no parameters. File '../parsed.json' will be executed.\n"
