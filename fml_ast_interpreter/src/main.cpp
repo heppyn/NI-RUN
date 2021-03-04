861be73ca9dcfdc297dfe067ea1c434ad906fb6d
Lukáš Hepner <hepneluk@fit.cvut.cz>
2021-03-04 22:04:54 +0100

    Add run parameter

--- fml_ast_interpreter/src/main.cpp
+++ fml_ast_interpreter/src/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char** argv) {
                       << "Execute file by executable -r <file path>\n";
             return 0;
         }
-        if (strcmp(argv[1], "-r") == 0) {
+        if (strcmp(argv[1], "-r") == 0 || strcmp(argv[1], "run") == 0) {
             if (argc == 2) {
                 std::cout << "Specify file to be executed.\n";
                 return 1;
